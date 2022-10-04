82a6ddfff8eacd55b14fd830bbb8821f1809ce5d
Mario Fe <mrodas@techeraperu.com>
2022-10-04 10:37:16 -0500

    Initial Commit

---
+++ facilito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  facilito
+//
+//  Created by iMac Mario on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
